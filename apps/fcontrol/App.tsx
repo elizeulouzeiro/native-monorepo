d6b098089fc91f3c77e468ba59105af9f9960bb0
elizeumatheus <elizeumatheus16@gmail.com>
2020-07-23 00:16:57 -0300

    chore: created react-native app

---
+++ apps/fcontrol/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Button} from '@native/uikit';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Button>asdsa</Button>
+    </SafeAreaView>
+  );
+};
+
+export default App;
