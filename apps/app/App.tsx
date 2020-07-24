4b117c8629176e9c0a47ca5f99d7c907658e6ddc
elizeumatheus <elizeumatheus16@gmail.com>
2020-07-23 22:18:43 -0300

    feat: created react native app

---
+++ apps/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Button} from '@native/uikit';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Button>oi</Button>
+    </SafeAreaView>
+  );
+};
+
+export default App;
